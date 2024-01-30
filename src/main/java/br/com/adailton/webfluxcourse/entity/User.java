c39c11357e8adab4c3d38e8ed1be7556613ae462
Adailton Junior <adailton.jr@gmail.com>
2024-01-30 00:23:40 -0300

    add user entity

---
+++ src/main/java/br/com/adailton/webfluxcourse/entity/User.java
@@ -0,0 +1,21 @@
+package br.com.adailton.webfluxcourse.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+public class User {
+
+    @Id
+    private String userId;
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+}
