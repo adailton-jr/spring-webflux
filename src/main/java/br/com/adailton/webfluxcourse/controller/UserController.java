a37028f63ad8e42beff2ee83659d8b560237479f
Adailton Junior <adailton.jr@gmail.com>
2024-01-30 00:56:55 -0300

    add request and response clases intecafes

--- src/main/java/br/com/adailton/webfluxcourse/controller/UserController.java
+++ src/main/java/br/com/adailton/webfluxcourse/controller/UserController.java
@@ -1,5 +1,7 @@
 package br.com.adailton.webfluxcourse.controller;
 
+import br.com.adailton.webfluxcourse.model.request.UserRequest;
+import br.com.adailton.webfluxcourse.model.response.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -16,7 +18,7 @@ public interface UserController {
     ResponseEntity<Flux<UserResponse>> findAll();
 
     @PatchMapping(value = "/{id}")
-    ResponseEntity<Mono<UseResponse>> update(@PathVariable String id, @RequestBody UserRequest request);
+    ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest request);
 
     @DeleteMapping(value = "/{id}")
     ResponseEntity<Mono<Void>> delete(@PathVariable String id);
