b79ab43d9f70df9d9163d375cd85a7894238258d
Adailton Junior <adailton.jr@gmail.com>
2024-02-22 02:41:55 -0300

    add: mapper e testado metodo save, ok

--- src/main/java/br/com/adailton/webfluxcourse/controller/impl/UserControllerImpl.java
+++ src/main/java/br/com/adailton/webfluxcourse/controller/impl/UserControllerImpl.java
@@ -3,6 +3,9 @@ package br.com.adailton.webfluxcourse.controller.impl;
 import br.com.adailton.webfluxcourse.controller.UserController;
 import br.com.adailton.webfluxcourse.model.request.UserRequest;
 import br.com.adailton.webfluxcourse.model.response.UserResponse;
+import br.com.adailton.webfluxcourse.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +13,16 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/users")
 public class UserControllerImpl implements UserController {
 
+    private final UserService service;
 
     @Override
     public ResponseEntity<Mono<Void>> save(UserRequest request) {
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.save(request).then());
     }
 
     @Override
--- src/main/java/br/com/adailton/webfluxcourse/service/UserService.java
+++ src/main/java/br/com/adailton/webfluxcourse/service/UserService.java
@@ -1,12 +1,21 @@
 package br.com.adailton.webfluxcourse.service;
 
 import br.com.adailton.webfluxcourse.entity.User;
+import br.com.adailton.webfluxcourse.mapper.UserMapper;
 import br.com.adailton.webfluxcourse.model.request.UserRequest;
+import br.com.adailton.webfluxcourse.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Service
+@RequiredArgsConstructor
 public class UserService {
 
-    public Mono<User> save(final UserRequest request) {
+    private final UserRepository repository;
+    private final UserMapper mapper;
 
+    public Mono<User> save(final UserRequest request) {
+        return repository.save(mapper.toEntity(request));
     }
 }
