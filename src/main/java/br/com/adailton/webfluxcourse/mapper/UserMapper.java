a655326d495311c2ca9e24302c8b1ed5982d2a35
Adailton Junior <adailton.jr@gmail.com>
2024-09-16 18:19:03 -0300

    fix: adicionando o update

--- src/main/java/br/com/adailton/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/br/com/adailton/webfluxcourse/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import br.com.adailton.webfluxcourse.model.request.UserRequest;
 import br.com.adailton.webfluxcourse.model.response.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
@@ -17,6 +18,8 @@ public interface UserMapper {
     @Mapping(target = "userId", ignore = true)
     User toEntity(final UserRequest request);
 
+    @Mapping(target = "userId", ignore = true)
+    User toEntity(final UserRequest request, @MappingTarget final User entity);
 
     UserResponse toResponse(final User entity);
 
--- src/main/java/br/com/adailton/webfluxcourse/service/UserService.java
+++ src/main/java/br/com/adailton/webfluxcourse/service/UserService.java
@@ -27,4 +27,10 @@ public class UserService {
     public Flux<User> findAll() {
         return repository.findAll();
     }
+
+    public Mono<User> update(final String id, final UserRequest request) {
+        return findById(id)
+                .map(user -> mapper.toEntity(request, user))
+                .flatMap(repository::save);
+    }
 }
