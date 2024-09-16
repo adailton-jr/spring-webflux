03bd8070315389fd93e501a631a648c819d05f15
Adailton Junior <adailton.jr@gmail.com>
2024-09-16 19:02:59 -0300

    fix: adicionando o handleNotFound generico para os metodos da classe UserService

--- src/main/java/br/com/adailton/webfluxcourse/service/UserService.java
+++ src/main/java/br/com/adailton/webfluxcourse/service/UserService.java
@@ -24,12 +24,7 @@ public class UserService {
     }
 
     public Mono<User> findById(final String id) {
-        return repository.findById(id)
-                .switchIfEmpty(Mono.error(
-                        new ObjectNotFoundException(
-                                format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName())
-                        )
-                ));
+        return handleNotFound(repository.findById(id), id);
 
     }
 
@@ -44,7 +39,11 @@ public class UserService {
     }
 
     public Mono<User> delete(String id) {
-        return repository.findAndRemove(id)
+        return handleNotFound(repository.findAndRemove(id), id);
+    }
+
+    private <T> Mono<T> handleNotFound(Mono<T> mono, String id) {
+        return mono
                 .switchIfEmpty(Mono.error(
                         new ObjectNotFoundException(
                                 format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName())
